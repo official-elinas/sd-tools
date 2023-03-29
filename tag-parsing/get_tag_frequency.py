1df6afe1c6269070da28ba4febc8c55180b33751
Elinas <support@sneerz.com>
2023-03-29 17:55:53 -0500

    add tag frequency script

---
+++ tag-parsing/get_tag_frequency.py
@@ -0,0 +1,26 @@
+import argparse
+import os
+from collections import defaultdict
+
+parser = argparse.ArgumentParser(description='Get the tag frequency in numerical order and write to a file.')
+parser.add_argument('-i', '--input_dir', required=True, help='tag input directory')
+args = parser.parse_args()
+
+counts = defaultdict(int)
+filecount = 0
+for file_name in os.listdir(args.input_dir):
+    if file_name.endswith('.txt'):
+        with open(os.path.join(args.input_dir, file_name), encoding="utf8") as f:
+            filecount += 1
+            first_line = f.readline().strip()
+            items = first_line.split(',')
+            # Iterate through the items and update the counts, starting at the second item
+            for item in items[1:]:
+                counts[item] += 1
+
+# Sort the counts in descending order of frequency and print the results
+with open('found_tags.txt', 'w', encoding='utf-8') as tag_file:
+    for item, count in sorted(counts.items(), key=lambda x: x[1], reverse=True):
+        write_format = f'{item}: {count}\n'
+        tag_file.write(write_format)
+    # print(f'{item}: {round(count/filecount, 2)*100}%')
